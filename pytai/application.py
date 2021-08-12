edbfa2a2a30da07e80c49cf2158f376b4b449766
Dvd848 <Dvd848@users.noreply.github.com>
2021-08-12 11:16:03 +0300

    Add license to files

--- pytai/application.py
+++ pytai/application.py
@@ -1,8 +1,36 @@
+"""The 'Controller' module, connecting between the 'View' and 'Model'.
+
+The application module acts as the 'Controller' and is responsible for
+connecting between the 'View' and the 'Model' in the MVC pattern.
+
+It receives user-triggered events from the View via callbacks, translates
+them to operations which need to be performed by the Model, and updates
+the View when the operations are completed.
+
+License:
+    MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
 from pathlib import Path
 from collections import namedtuple
-from pytai.view.events import Events
 from typing import Union, Dict
-import inspect
 
 from . import view as v
 from . import model as m
