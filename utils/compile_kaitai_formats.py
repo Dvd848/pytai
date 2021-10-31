af6132759793e65243cb75169d0094d6259927d8
Dvd848 <Dvd848@users.noreply.github.com>
2021-10-31 23:07:04 +0200

    Add missing comments in license

--- utils/compile_kaitai_formats.py
+++ utils/compile_kaitai_formats.py
@@ -495,9 +495,9 @@ LICENSE_CC0_10 = """
 """
 
 LICENSE_WTFPL = """
-This work is free. You can redistribute it and/or modify it under the
-terms of the Do What The Fuck You Want To Public License, Version 2,
-as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 """
 
 LICENSE_UNLICENSE = """
