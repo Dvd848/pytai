2db2518be9c4324aed182f36208c0e0ccf53a96c
Ange <ange.albertini@gmail.com>
2021-10-31 17:07:25 +0100

    missing comments

--- pytai/kaitai/formats/ines.py
+++ pytai/kaitai/formats/ines.py
@@ -1,6 +1,6 @@
-This work is free. You can redistribute it and/or modify it under the
-terms of the Do What The Fuck You Want To Public License, Version 2,
-as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 
 # This file was compiled from a KSY format file downloaded from:
 # https://github.com/kaitai-io/kaitai_struct_formats
