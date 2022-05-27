80c88e8842d78492e62e8d5a52fc5e40eabf8ac2
Dvd848 <Dvd848@users.noreply.github.com>
2022-05-27 22:17:58 +0300

    Fix packing the project to a pyz file

---
+++ __main__.py
@@ -0,0 +1,36 @@
+"""A basic Visualizer and HEX Viewer GUI in Python, based on Kaitai Struct parsing.
+
+This program offers a basic user interface to analyze the internal structure of 
+binary files for a number of predefined formats. 
+It utilizes the Kaitai Struct project to allow the user to navigate through 
+a given binary file and match its logical contents to the matching binary
+representation as displayed in the HEX view.
+
+Source:
+    https://github.com/Dvd848/pytai
+
+License:
+    MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+from pytai.main import main
+
+if __name__ == "__main__":
+    main()
